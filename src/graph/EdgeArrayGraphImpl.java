207ff16f30eba510ccea4a2098d025677bef4e8f
agustinVon <agustin.von@ing.austral.edu.ar>
2020-08-29 04:17:09 -0300

    Edge array graph implementation completed.

--- src/graph/EdgeArrayGraphImpl.java
+++ src/graph/EdgeArrayGraphImpl.java
@@ -1,56 +1,93 @@
 package graph;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO: implement
 public class EdgeArrayGraphImpl<T> implements Graph<T> {
+
+    private final ArrayList<T> vertex= new ArrayList<>();
+    private int alfa=0;
+    private int n=0;
+
+    private final List<T[]> edgeArray=new ArrayList<>();
+
     @Override
     public void addVertex(T x) {
-        throw new UnsupportedOperationException("TODO");
+        vertex.add(x);
+        n++;
     }
 
     @Override
     public boolean hasVertex(T v){
-        throw new UnsupportedOperationException("TODO");
+        return vertex.contains(v);
     }
 
     @Override
     public void removeVertex(T x) {
-        throw new UnsupportedOperationException("TODO");
+        if(!hasVertex(x)){
+            throw new RuntimeException();
+        }
+        else{
+            vertex.remove(x);
+            n--;
+            edgeArray.removeIf(node -> node[0].equals(x));
+            edgeArray.removeIf(node -> node[1].equals(x));
+        }
     }
 
     @Override
     public void addEdge(T v, T w) {
-        throw new UnsupportedOperationException("TODO");
+        if(!hasVertex(v)||!hasVertex(w)) return;
+        else{
+            T[] node =(T[])new Object[2];
+            node[0]=v;
+            node[1]=w;
+            edgeArray.add(node);
+            alfa++;
+        }
     }
 
     @Override
     public void removeEdge(T v, T w) {
-        throw new UnsupportedOperationException("TODO");
+        edgeArray.removeIf(node -> node[0].equals(v) && node[1].equals(w) || node[0].equals(w) && node[1].equals(v));
+        alfa--;
     }
 
     @Override
     public boolean hasEdge(T v, T w) {
-        throw new UnsupportedOperationException("TODO");
+        for (T[] node:edgeArray) {
+            if ((node[0].equals(v)&&node[1].equals(w))||(node[0].equals(w)&&node[1].equals(v))) return true;
+        }
+        return false;
     }
 
     @Override
     public int order() {
-        throw new UnsupportedOperationException("TODO");
+        return n;
     }
 
     @Override
     public int alpha() {
-        throw new UnsupportedOperationException("TODO");
+        return alfa;
     }
 
     @Override
     public List<T> getVertexes() {
-        throw new UnsupportedOperationException("TODO");
+        return vertex;
     }
 
     @Override
     public List<T> getAdjacencyList(T v) {
-        throw new UnsupportedOperationException("TODO");
+        List<T> adjacencyList=new ArrayList<>();
+        for (T[] node:edgeArray) {
+            if(node[0].equals(v)){
+                adjacencyList.add(node[1]);
+            }
+            else if (node[1].equals(v)){
+                adjacencyList.add(node[0]);
+            }
+        }
+        return adjacencyList;
     }
 }
